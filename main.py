e2cd321cbeb1d17e8518a3165e47451416e27573
Daniele Orzechowski <daniorzechowski@gmail.com>
2025-02-14 22:44:39 -0300

    feat: mais documentacao

--- main.py
+++ main.py
@@ -1,7 +1,11 @@
 from fastapi import FastAPI
 from routes.movies import router as movies_router
 
-app = FastAPI(title="Sistema de Recomendação Inteligente de Filmes")
+app = FastAPI(
+    title="Sistema de Recomendação Inteligente de Filmes",
+    description="Uma API para recomendar filmes com base em similaridade de gêneros e analisar sentimentos das críticas.",
+    version="1.0.0",
+)
 
 # Registrar as rotas dos filmes
 app.include_router(movies_router)
